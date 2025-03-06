2e33cb1734a52f8a9d0404c062c7363d57fb0a8a
Colin McFadden <mcfadden.colin@gmail.com>
2025-03-06 11:41:23 -0500

    lsp and numbering updates

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -4,7 +4,9 @@ local o = vim.opt
 -- Editor options
 
 o.number = true
-
+o.number = true
+o.relativenumber = true
+o.signcolumn = "number"
 -- Remaps
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,96 +1,161 @@
--- Adapted from a combo of
--- https://lsp-zero.netlify.app/v3.x/blog/theprimeagens-config-from-2022.html
--- https://github.com/ThePrimeagen/init.lua/blob/master/lua/theprimeagen/lazy/lsp.lua
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "j-hui/fidget.nvim",
-  },
-  config = function()
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
+	{
+		"williamboman/mason.nvim",
+		-- event = "VeryLazy",
+		lazy = true,
+		config = function()
+			require('mason').setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		-- event = "VeryLazy",
+		lazy = true,
+		dependencies = {
+			"williamboman/mason.nvim"
+		},
+		opts = {
+			auto_install = true,
+		},
+		config = function()
+			require('mason-lspconfig').setup({
+				ensure_installed = { "lua_ls", "omnisharp" }
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		-- event = "VeryLazy",
+		-- lazy = true,
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local lspconfig = require('lspconfig')
 
-    require("fidget").setup({})
-    require("mason").setup()
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
 
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        "quick_lint_js",
-        "lua_ls",
-        "omnisharp"
-      },
-      handlers = {
-        function(server_name)
-          require('lspconfig')[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-        lua_ls = function()
-          require('lspconfig').lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                runtime = {
-                  version = 'LuaJIT'
-                },
-                diagnostics = {
-                  globals = { 'vim', 'love' },
-                },
-                workspace = {
-                  library = {
-                    vim.env.VIMRUNTIME,
-                  }
-                }
-              }
-            }
-          })
-        end,
-	omnisharp = function()
-		require('lspconfig').omnisharp.setup({
-		})
-	end
-      }
-    })
+			lspconfig.omnisharp.setup({
+				capabilities = capabilities,
+				enable_roslyn_analysers = true,
+				enable_import_completion = true,
+				organize_imports_on_format = true,
+				enable_decompilation_support = true,
+				filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets' }
+			})
 
-    local cmp = require('cmp')
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+			lspconfig.powershell_es.setup({
+				capabilities = capabilities,
+				bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+				init_options = {
+					enableProfileLoading = false,
+				}
+			})
 
-    -- this is the function that loads the extra snippets to luasnip
-    -- from rafamadriz/friendly-snippets
-    require('luasnip.loaders.from_vscode').lazy_load()
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+			})
 
-    cmp.setup({
-      sources = {
-        { name = 'path' },
-        { name = 'nvim_lsp' },
-        { name = 'luasnip', keyword_length = 2 },
-        { name = 'buffer',  keyword_length = 3 },
-      },
-      mapping = cmp.mapping.preset.insert({
-        -- ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        -- ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        -- ['<C-Space>'] = cmp.mapping.complete(),
-      }),
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
-    })
-  end
+			lspconfig.yamlls.setup({
+				capabilities = capabilities
+			})
+
+			lspconfig.buf_ls.setup({
+				capabilities = capabilities
+			})
+
+			lspconfig.bicep.setup({
+				capabilities = capabilities
+			})
+
+			lspconfig.lemminx.setup({
+				capabilities = capabilities
+			})
+
+			lspconfig.pylsp.setup({
+				capabilities = capabilities
+			})
+
+			-- lspconfig.codeql.setup({
+			-- 	capabilities = capabilities
+			-- })
+
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			-- vim.keymap.set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, {})
+		end
+	},
+	{
+		'nvimtools/none-ls.nvim',
+		-- event = { "BufReadPre", "BufNewFile" },
+		lazy = true,
+		-- event = "VeryLazy",
+		config = function()
+			local null_ls = require('null-ls')
+			null_ls.setup({
+				sources = {
+					-- null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.csharpier,
+					null_ls.builtins.formatting.yamlfmt,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+				}
+			})
+			vim.keymap.set('n', '<leader>lff', function() vim.lsp.buf.format({ async = true }) end,
+				{ desc = "Format document" })
+			vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = "Rename Symbol" })
+			vim.keymap.set({ 'n', 'i' }, '<f2>', vim.lsp.buf.rename, { desc = "Rename Symbol" })
+			vim.keymap.set({ 'n', 'i' }, '<f12>', vim.lsp.buf.definition, { desc = "Go to Definition" })
+			vim.keymap.set({ 'n' }, '<leader>ld', vim.lsp.buf.definition, { desc = "Go to Definition" })
+			vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+			vim.keymap.set('n', '<leader>lh', vim.lsp.buf.signature_help, { desc = "Signature Help" })
+			vim.keymap.set('n', '<leader>lsR', vim.lsp.buf.references, { desc = "To to References" })
+			-- vim.keymap.set({ 'n' }, '<leader>lsD', ":Trouble document_diagnostics<CR>", { desc = "Toggle Document Diagnostics" })
+			vim.keymap.set({ 'n' }, '<leader>lsD', ":Trouble diagnostics<CR>", { desc = "Toggle Document Diagnostics" })
+			vim.keymap.set('n', '<leader>lsI', ':Trouble lsp_implementations<CR>',
+				{ desc = "Toggle LSP References" })
+			vim.keymap.set('n', '<leader>lsd', ":Trouble lsp_definitions<CR>", { desc = "Toggle LSP Definitions" })
+		end
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		-- event = { 'VeryLazy' },
+		-- enabled = false,
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+			-- "neovim/nvim-lspconfig"
+		},
+		config = function()
+			require('mason-null-ls').setup({
+				automatic_setup = true
+			})
+		end,
+	},
+	{
+		'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+		event = "VeryLazy",
+		config = function()
+			require('lsp_lines').setup()
+
+			vim.diagnostic.config({
+				virtual_lines = false,
+				virtual_text = true,
+			})
+
+			local function toggleLines()
+				local new_value = not vim.diagnostic.config().virtual_lines
+				vim.diagnostic.config({ virtual_lines = new_value, virtual_text = not new_value })
+				return new_value
+			end
+
+			vim.keymap.set('n', '<leader>lu', toggleLines, { desc = "Toggle Underline Diagnostics", silent = true })
+		end
+	},
 }
